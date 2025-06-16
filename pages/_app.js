abf3c9ba04aec5232e4739924f169bde25c5f490
ORiVS <107984700+ORiVS@users.noreply.github.com>
2025-06-16 17:43:18 +0200

    add logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            <Head>
+                <link rel="icon" href="/favicon.png" />
+                <title>Sessime Orias</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 };
 
 export default App;
